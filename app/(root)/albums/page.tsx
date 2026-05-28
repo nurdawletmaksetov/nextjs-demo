da588626a320e71602f85617ca68b57cea7e4f3a
Nurdawlet Maksetov <nurdawletmaksetov@gmail.com>
2026-05-28 09:32:02 +0500

    data fetching

---
+++ app/(root)/albums/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+async function Albums () {
+    const response = await fetch('http://jsonplaceholder.typicode.com/albums');
+    if (!response.ok) throw Error('Failed to fetch data');
+
+    const albums = await response.json();
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols">
+            {albums.map((album: {id: number; title: string}) => (
+                <div key={album.id} className={'bg-white shadow-md rounded-lg p-4 transition t...'}>
+                    <h3 className={"text-lg font-bold mb-2"}>{album.title}</h3>
+                    <p className={"text-gray-600"}>Album ID: {album.id}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+export default Albums
